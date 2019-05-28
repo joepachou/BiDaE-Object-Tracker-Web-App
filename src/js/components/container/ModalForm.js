3f773b9e0efa3216c5598e7b74644bc0e2d9ee68
JaneLab <janelab@JaneLabdeiMac.local>
2019-05-28 12:05:52 +0800

    refine modal form

--- src/js/components/container/ModalForm.js
+++ src/js/components/container/ModalForm.js
@@ -8,22 +8,35 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import FormCheck from 'react-bootstrap/FormCheck';
 import VerticalTable from '../presentational/VerticalTable';
+import Select from 'react-select';
 
+const options = [
+    { value: 'chocolate', label: 'Chocolate' },
+    { value: 'strawberry', label: 'Strawberry' },
+    { value: 'vanilla', label: 'Vanilla' }
+  ];
+  
 
 class ModalForm extends React.Component {
     
     constructor(props) {
         super(props);
-    
+
+        this.state = {
+            show: this.props.show,
+            isShowForm: false,
+            formOption: {
+                checkedStatus: 'Normal', 
+                selectedOption: null,
+            }
+        };
+
+
         this.handleShow = this.handleShow.bind(this);
         this.handleClose = this.handleClose.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
-    
-        this.state = {
-            show: this.props.show,
-            editObjectFormIsShow: false,
-            
-        };
+        this.handleCheck = this.handleCheck.bind(this);
+        this.handleSelect = this.handleSelect.bind(this);
     }
   
     handleClose() {
@@ -42,21 +55,42 @@ class ModalForm extends React.Component {
         if (prevProps != this.props) {
             this.setState({
                 show: this.props.show,
-                editObjectFormIsShow: true,
+                isShowForm: true,
             })
         }
     }
 
     handleSubmit(e) {
-        console.log(this);
+        console.log(this.state.formOption);
 
         e.preventDefault();
+    }
+
+    handleCheck(e) {
+        console.log(this.state)
+        this.setState({
+            formOption: {
+                ...this.state.formOption,
+                checkedStatus: e.target.value,
+            }
+        })
+    }
+
+    handleSelect(selectedOption) {
+        console.log(this.state)
+        this.setState({
+            formOption: {
+                selectedOption: selectedOption.value,
+            }
+        })
     }
 
   
     render() {
 
         const { title, selectedObjectData } = this.props;
+        const { checkedStatus, selectedOption } = this.state.formOption;
+
 
         const style = {
             button: {
@@ -67,8 +101,7 @@ class ModalForm extends React.Component {
         return (
             <>
                 {/* <Button onClick={this.handleShow} variant="primary" style={style.button}>{title}</Button> */}
-                <Modal show={this.state.show} onHide={this.handleClose} >
-                {console.log(selectedObjectData)}
+                <Modal show={this.state.show} onHide={this.handleClose} size="lg" >
                     <Modal.Header>{title}</Modal.Header>
                     <Modal.Body>
                         <Form onSubmit={this.handleSubmit}>
@@ -85,7 +118,7 @@ class ModalForm extends React.Component {
                                     Device Type
                                 </Form.Label>
                                 <Col sm={7} >
-                                    <Form.Control type="text" placeholder={selectedObjectData ? selectedObjectData.name : null} disabled/>
+                                    <Form.Control type="input" placeholder={selectedObjectData ? selectedObjectData.name : null} disabled/>
                                 </Col>
                             </Form.Group>
                             <Form.Group as={Row}>
@@ -103,14 +136,15 @@ class ModalForm extends React.Component {
                                         Status
                                     </Form.Label>
                                     <Col sm={8}>
-                                    
                                         <Form.Check
                                             custom
                                             type="radio"
                                             label="Normal"
                                             name="formHorizontalRadios"
                                             id="formHorizontalRadios1"
-                                            
+                                            value="Normal"
+                                            checked={checkedStatus === 'Normal'}
+                                            onChange={this.handleCheck}                                     
                                         />
                                         <Form.Check
                                             custom
@@ -118,21 +152,41 @@ class ModalForm extends React.Component {
                                             label="Broken"
                                             name="formHorizontalRadios"
                                             id="formHorizontalRadios2"
+                                            value="Broken"
+                                            checked={checkedStatus === 'Broken'}
+
+                                            onChange={this.handleCheck}   
                                         />
-                                        <Form.Check
-                                            custom
-                                            type="radio"
-                                            label="Transferred"
-                                            name="formHorizontalRadios"
-                                            id="formHorizontalRadios2"
-                                        />
-                                        <select class="custom-select custom-select-lg mb-3" disabled>
-  <option selected>Open this select menu</option>
-  <option value="1">One</option>
-  <option value="2">Two</option>
-  <option value="3">Three</option>
-</select>
+                                        <Form.Row>
+                                            <Form.Group as={Col}>
+                                                <Form.Check
+                                                    custom
+                                                    type="radio"
+                                                    label="Transferred"
+                                                    name="formHorizontalRadios"
+                                                    id="formHorizontalRadios3"
+                                                    value="Transferred"
+                                                    checked={checkedStatus === 'Transferred'}
+
+                                                    onChange={this.handleCheck}   
+                                                />
+                                            </Form.Group>
+                                            <Form.Group as={Col}>
+                                                {/* <Form.Control as="select" disabled = {checkedStatus === "Transferred" ? false : true } onChange={this.handleCheck} >
+                                                    <option>Choose...</option>
+                                                    <option>...</option>
+                                                </Form.Control> */}
+                                                {console.log(selectedOption)}
+                                                    <Select
+                                                        placeholder = "Select Location"
+                                                        value = {selectedOption}
+                                                        onChange={this.handleSelect}
+                                                        options={options}
+                                                        isDisabled = {checkedStatus === 'Transferred' ? false : true}
+                                                    />
+                                            </Form.Group>
                                             
+                                        </Form.Row>
                                     </Col>
                                 </Form.Group>
                              </fieldset>
