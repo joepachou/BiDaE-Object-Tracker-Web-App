fdf104ccdd11a0b32c71f4c815fda5e5d5f74e7f
chunyu1202 <chunyu1202@gmail.com>
2019-05-29 23:11:21 +0800

    Refine SQL query string

--- queryType.js
+++ queryType.js
@@ -1,67 +1,99 @@
 
 function query_getTrackingData (rssi = -55) {
 	return `
-	SELECT table_location.object_mac_address, table_device.name, table_location.lbeacon_uuid, table_location.avg as avg, table_location.avg_stable as avg_stable, table_location.panic_button as panic_button, table_location.geofence_type as geofence_type 
+	SELECT table_location.object_mac_address, 
+	       table_device.name, 
+		   table_location.lbeacon_uuid, 
+		   table_location.avg as avg, 
+		   table_location.avg_stable as avg_stable, 
+		   table_location.panic_button as panic_button, 
+		   table_location.geofence_type as geofence_type 
 	FROM
-	(
-	SELECT table_track_data.object_mac_address, table_track_data.lbeacon_uuid, table_track_data.avg as avg, table_track_data.avg_stable as avg_stable, table_panic.panic_button as panic_button, NULL as geofence_type 
-	     FROM
-		(SELECT table_recent.object_mac_address, table_recent.lbeacon_uuid, table_recent.rssi as avg, table_stable.avg_stable as avg_stable 
-		 FROM 
-	        (SELECT object_mac_address, lbeacon_uuid, round(avg(rssi),2) as rssi
-		     FROM tracking_table
-		        WHERE final_timestamp >= NOW() - INTERVAL '10 seconds'  
-		        AND final_timestamp >= NOW() - (server_time_offset||' seconds')::INTERVAL - INTERVAL '3 seconds'  
-		        GROUP BY object_mac_address, lbeacon_uuid
-				HAVING avg(rssi) > -65
-		    ) as table_recent 
+	
+	    (
+	    SELECT table_track_data.object_mac_address, 
+		       table_track_data.lbeacon_uuid, 
+			   table_track_data.avg as avg, 
+			   table_track_data.avg_stable as avg_stable, 
+			   table_panic.panic_button as panic_button, 
+			   NULL as geofence_type 
+	    FROM
 		
+		    (
+			SELECT table_recent.object_mac_address, 
+			       table_recent.lbeacon_uuid, 
+				   table_recent.rssi as avg, 
+				   table_stable.avg_stable as avg_stable    
+		    FROM 
+			
+	            (
+				SELECT object_mac_address, 
+				       lbeacon_uuid, 
+					   round(avg(rssi),2) as rssi
+		        FROM tracking_table
+		            WHERE final_timestamp >= NOW() - INTERVAL '10 seconds'  
+		            AND final_timestamp >= NOW() - (server_time_offset||' seconds')::INTERVAL - INTERVAL '3 seconds'  
+		            GROUP BY object_mac_address, lbeacon_uuid
+				    HAVING avg(rssi) > -65
+		        ) as table_recent 
+		
+		        LEFT JOIN
+		
+		        (
+				SELECT object_mac_address, 
+				       lbeacon_uuid, 
+					   round(avg(rssi),2) as avg_stable
+		        FROM tracking_table 
+		            WHERE final_timestamp >= NOW() - INTERVAL '70 seconds'
+		            AND final_timestamp >= NOW() - (server_time_offset||' seconds')::INTERVAL - INTERVAL '60 seconds' 
+		            GROUP BY object_mac_address, lbeacon_uuid
+		            HAVING avg(rssi) > -65 and count(object_mac_address) >= 40
+		        ) as table_stable 
+		
+		        ON table_recent.object_mac_address = table_stable.object_mac_address 
+		        AND table_recent.lbeacon_uuid = table_stable.lbeacon_uuid
+            ) as table_track_data	    
+		     
 		    LEFT JOIN
 		
-		    (SELECT object_mac_address, lbeacon_uuid, round(avg(rssi),2) as avg_stable
-		     FROM tracking_table 
-		        WHERE final_timestamp >= NOW() - INTERVAL '70 seconds'
-		        AND final_timestamp >= NOW() - (server_time_offset||' seconds')::INTERVAL - INTERVAL '60 seconds' 
-		        GROUP BY object_mac_address, lbeacon_uuid
-		        HAVING avg(rssi) > -65 and count(object_mac_address) >= 40
-		    ) as table_stable 
+		    (
+			SELECT object_mac_address, 
+			       lbeacon_uuid, 
+				   max(panic_button) as panic_button 
+		    FROM tracking_table
+                WHERE final_timestamp >= NOW() - INTERVAL '190 seconds'
+                AND final_timestamp >= NOW() - (server_time_offset||' seconds')::INTERVAL - INTERVAL '180 seconds'
+                GROUP BY object_mac_address, lbeacon_uuid			
+		        HAVING max(panic_button) > 0
+		    ) as table_panic	
 		
-		    ON table_recent.object_mac_address = table_stable.object_mac_address 
-		    AND table_recent.lbeacon_uuid = table_stable.lbeacon_uuid
-        ) as table_track_data	    
-		     
-		LEFT JOIN
-		
-		(SELECT object_mac_address, lbeacon_uuid, max(panic_button) as panic_button 
-		 FROM tracking_table
-            WHERE final_timestamp >= NOW() - INTERVAL '190 seconds'
-            AND final_timestamp >= NOW() - (server_time_offset||' seconds')::INTERVAL - INTERVAL '180 seconds'
-            GROUP BY object_mac_address, lbeacon_uuid			
-		    HAVING max(panic_button) > 0
-		) as table_panic	
-		
-		ON table_track_data.object_mac_address = table_panic.object_mac_address 
-		AND table_track_data.lbeacon_uuid = table_panic.lbeacon_uuid
+		    ON table_track_data.object_mac_address = table_panic.object_mac_address 
+		    AND table_track_data.lbeacon_uuid = table_panic.lbeacon_uuid
 	
+	    UNION
 	
-	UNION
-	
-	SELECT mac_address as object_mac_address, uuid as lbeacon_uuid, MAX(rssi) as avg, NULL as avg_table, NULL panic_button, type as geofence_type 
-	 FROM geo_fence_alert 
-	     WHERE receive_time >= NOW() - INTERVAL '5 seconds'
-		 GROUP BY mac_address, uuid, type
+	    SELECT mac_address as object_mac_address, 
+		       uuid as lbeacon_uuid, 
+			   MAX(rssi) as avg, 
+			   NULL as avg_table,
+			   NULL panic_button, 
+			   type as geofence_type 
+	    FROM geo_fence_alert 
+	        WHERE receive_time >= NOW() - INTERVAL '5 seconds'
+		    GROUP BY mac_address, uuid, type
     
-	ORDER BY object_mac_address ASC, lbeacon_uuid ASC
+	        ORDER BY object_mac_address ASC, lbeacon_uuid ASC
      
-	) as table_location
+	    ) as table_location
 	
-	LEFT JOIN 
+	    LEFT JOIN 
 	
-	(SELECT mac_address, name
-	 FROM object_table
-	) as table_device
+	    (
+		SELECT mac_address, name
+	    FROM object_table
+	    ) as table_device
 	
-	ON table_location.object_mac_address = table_device.mac_address
+	    ON table_location.object_mac_address = table_device.mac_address
     
 	`;
 }
