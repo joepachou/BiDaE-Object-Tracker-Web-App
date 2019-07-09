047925eb076f4467adb3b17a861fec097b0c8d44
JaneLab <janelab@DHCP-22241.iis.sinica.edu.tw>
2019-07-09 17:15:26 +0800

    add the babel-polyfill

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var path = require('path')
 
 
 module.exports = {
-    entry: "./src/index.js",
+    entry: ['babel-polyfill', './src/index.js'],
     mode: 'development',
     output: {
         path: path.join(__dirname, 'dist'),
