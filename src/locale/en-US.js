17401ca176e0e05d6bb806459181bb20551b50a8
JaneLab <janelab@JaneLabdeiMac.local>
2019-08-26 10:34:20 +0800

    update locale package

--- src/locale/en-US.js
+++ src/locale/en-US.js
@@ -1,21 +1,17 @@
 const en =  {
     ABBR: 'en-US',
 
-    surveillance: 'Surveillance',
     HOME: 'home',
     HEALTH_REPORT: 'health report',
     GEOFENCE: 'geofence',
     OBJECT_MANAGEMENT: 'object management',
-    object_types: 'object types',
-    search: 'search',
-    language: 'language',
-    search_result: 'search result',
 
-
-    /* Frequent Search */
+    SEARCH_RESULT: 'search result',
+    No_RESULT: 'no result',
     FREQUENT_SEARCH: 'frequent searches',
-    ALL_DEVICE: 'All Devices',
-    MY_DEVICE: 'My Devices',
+    FOUND: 'found',
+    NOT_FOUND: 'not found',
+
 
     /* Location accuracy */
     LOCATION_ACCURACY: 'Location Accuracy',
@@ -23,51 +19,27 @@ const en =  {
     MED: 'med',
     HIGH: 'high',
 
-    // clear and save
+    /* Frequent Search */
+    ALL_DEVICE: 'All Devices',
+    MY_DEVICE: 'My Devices',
+
+    /* Button */
     CLEAR : 'clear',
     SAVE : 'save',
-    
-    /* Search related term */
-    Yunlin_Christian_Hospital: 'Yunlin Christian Hospital',
-    NTU_Hospital_Yunlin: 'NTU Hospital Yunlin Branch',
-    NTU_Hospital_Taipe: 'NTU Hospital Taipe Branch',
-
+    CANCEL: 'cancel',
+    SEND: 'send',
     SIGN_IN: 'sign in',
     SIGN_UP: 'sign up',
     SIGN_OUT: 'sign out',
-
-    EDIT_LBEACON: 'edit lbeacon',
-    ADD_OBJECT: 'add object',
-    EDIT_OBJECT: 'edit object',
-
-    /* Object Status */
-    NORMAL: 'normal',
-    BROKEN: 'broken',
-    TRANSFERRED: 'transferred',
-    RESERVE: 'reserve',
+    SHIFT_CHANGE_RECORD: 'shift change record',
+    SHOW_DEVICES: 'show devices',
+    HIDE_DEVICES: 'hide devices',
+    SHOW:'show',
+    DEVICE: 'device',
+    DEVICES: 'devices',
 
 
-    /* Form */
-    CANCEL: 'cancel',
-    SEND: 'send',
-
-    /* Time Scale*/
-    DAY: 'day',
-    HOUR: 'hour',
-    MINUTE: 'minute',
-    SECOND: 'second',
-
-    SEARCH_RESULT: 'search result',
-
-    SELECT_LOCATION: 'select location',
-
-    NAME_IS_REQUIRED: 'Name is required',
-    TYPE_IS_REQUIRED: 'Type is required',
-    ACCESS_CONTROL_NUMBER_IS_REQUIRED: 'Access control number is required',
-    MAC_ADDRESS_IS_REQUIRED: 'Mac address is required',
-    STATUS_IS_REQUIRED: 'Status is required',
-
-    /* Form field */
+    /* Form Field */
     NAME: 'name',
     TYPE: 'type',
     ACCESS_CONTROL_NUMBER: 'access control number',
@@ -75,19 +47,49 @@ const en =  {
     STATUS: 'status',
     MONITOR_TYPE: 'monitor type',
     ACN: 'acn',
+    SELECT_LOCATION: 'select location',
+    NAME_IS_REQUIRED: 'Name is required',
+    TYPE_IS_REQUIRED: 'Type is required',
+    ACCESS_CONTROL_NUMBER_IS_REQUIRED: 'Access control number is required',
+    MAC_ADDRESS_IS_REQUIRED: 'Mac address is required',
+    STATUS_IS_REQUIRED: 'Status is required',
 
-    FOUND: 'found',
+    /** Form Title */
+    EDIT_LBEACON: 'edit lbeacon',
+    ADD_OBJECT: 'add object',
+    EDIT_OBJECT: 'edit object',
+    REPORT_DEVICE_STATUS: 'Report device status',
+    THANK_YOU_FOR_REPORTING: 'Thank you for reporting',
 
-    SHIFT_CHANGE_RECORD: 'Shift Change Record',
+    /* Object Status */
+    STATUS: 'status',
+    NORMAL: 'normal',
+    BROKEN: 'broken',
+    TRANSFERRED: 'transferred',
+    RESERVE: 'reserve',
 
-    DEVICE_FOUND: function (length){
-        return length.toString()+' devices are found'
-    },
-    DEVICE_NOT_FOUND: function (length){
-        return length.toString()+' devices are not found'
-    },
+    /* Time Scale*/
+    DAY: 'day',
+    HOUR: 'hour',
+    MINUTE: 'minute',
+    SECOND: 'second',
+  
+    /* Search related term */
+    Yunlin_Christian_Hospital: 'Yunlin Christian Hospital',
+    NTU_Hospital_Yunlin: 'NTU Hospital Yunlin Branch',
+    NTU_Hospital_Taipe: 'NTU Hospital Taipe Branch',
 
-    SEARCH_RESULT: 'Search Results',
+   /** User Setting */
+   User_Setting: 'User Setting',
+   Devices_Management: 'Devices Management',
+   Shift_Record_History: 'Shift Change History',
+   Edit_Object_Management: 'Edit Object Record',
+   ACCESS_RIGHT: 'Access Right',
+   MY_DEVICES: 'My Devices',
+   OTHER_DEVICES: 'Other Devices',
+   ADMIN: 'Admin',
+   CONFIRM: 'Confirm', 
+   REMOVE_USER_CONFIRM: 'Remove User',
 
 }
 
--- src/locale/zw-tw.js
+++ src/locale/zw-tw.js
@@ -1,20 +1,16 @@
 const tw = {
     ABBR: 'zh-tw',
 
-    object_types: '可搜尋物件',
-    search: '搜尋',
-    language: '語言',
-    search_result: '搜尋結果',
-
-    /* for location accuracy */
     HOME : '首頁',
     HEALTH_REPORT: 'Health Report',
     GEOFENCE: 'Geofence',
     OBJECT_MANAGEMENT: '物件管理',
-    // frequent search
+
+    SEARCH_RESULT: '搜尋結果',
+    No_RESULT: 'no result',
     FREQUENT_SEARCH: '常用搜尋',
-    ALL_DEVICE: '全部儀器',
-    MY_DEVICE: '我的儀器',
+    FOUND: '找到',
+    NOT_FOUND: '未找到',
 
 
     /* Location accuracy */
@@ -22,33 +18,55 @@ const tw = {
     LOW: '低',
     MED: '中',
     HIGH: '高', 
+    
+    /* Frequent Search */
+    ALL_DEVICE: '全部儀器',
+    MY_DEVICE: '我的儀器',
 
-    // clear and save
+    /* Button */
     CLEAR : '清除',
     SAVE : '儲存',
-
-     // user Information
+    CANCEL: '取消',
+    SEND: '送出',
     SIGN_IN: '登入',
     SIGN_UP: '註冊',
     SIGN_OUT: '登出',
     SHIFT_CHANGE_RECORD: '交接記錄',
+    SHOW_DEVICES: '顯示儀器',
+    HIDE_DEVICES: '隱藏儀器',
+    SHOW:'顯示',
+    DEVICES: '儀器',
+    DEVICE: '儀器',
 
-    EDIT_LBEACON: 'edit Lbeacon',
+
+    /* Form Field */
+    NAME: 'name',
+    TYPE: 'type',
+    ACCESS_CONTROL_NUMBER: '財產編號',
+    MAC_ADDRESS: 'mac address',
+    STATUS: 'status',
+    MONITOR_TYPE: 'monitor type',
+    ACN: '財產編號',
+    SELECT_LOCATION: 'select location',
+    NAME_IS_REQUIRED: 'Name is required',
+    TYPE_IS_REQUIRED: 'Type is required',
+    ACCESS_CONTROL_NUMBER_IS_REQUIRED: 'Access control number is required',
+    MAC_ADDRESS_IS_REQUIRED: 'Mac address is required',
+    STATUS_IS_REQUIRED: 'Status is required',
+
+    /** Form Title */
+    EDIT_LBEACON: 'edit lbeacon',
     ADD_OBJECT: 'add object',
     EDIT_OBJECT: 'edit object',
+    REPORT_DEVICE_STATUS: 'Report device status',
+    THANK_YOU_FOR_REPORTING: 'Thank you for reporting',
 
     /* Object Status */
+    STATUS: 'status',
     NORMAL: 'normal',
     BROKEN: 'broken',
     TRANSFERRED: 'transferred',
     RESERVE: 'reserve',
-
-
-    STATUS: 'status',
-
-    /* Form */
-    CANCEL: '取消',
-    SEND: 'send',
 
     /* Time Scale*/
     DAY: 'day',
@@ -56,17 +74,22 @@ const tw = {
     MINUTE: 'minute',
     SECOND: 'second',
 
-    SEARCH_RESULT: 'search result',
+    /* Search related term */
+    Yunlin_Christian_Hospital: 'Yunlin Christian Hospital',
+    NTU_Hospital_Yunlin: 'NTU Hospital Yunlin Branch',
+    NTU_Hospital_Taipe: 'NTU Hospital Taipe Branch',
 
-    SELECT_LOCATION: 'select location',
-
-    NAME_IS_REQUIRED: 'Name is required',
-    TYPE_IS_REQUIRED: 'Type is required',
-    ACCESS_CONTROL_NUMBER_IS_REQUIRED: 'Access control number is required',
-    MAC_ADDRESS_IS_REQUIRED: 'Mac address is required',
-    STATUS_IS_REQUIRED: 'Status is required',
-
-
+    /** User Setting */
+    User_Setting: '帳戶設定',
+    Devices_Management: '儀器管理',
+    Shift_Record_History: '換班紀錄',
+    Edit_Object_Management: '儀器狀態更改紀錄',
+    ACCESS_RIGHT: '使用者權限',
+    MY_DEVICES: '我的儀器',
+    OTHER_DEVICES: '其他儀器',
+    ADMIN: 'Admin',
+    CONFIRM: 'Confirm', 
+    REMOVE_USER_CONFIRM: 'Remove User',
 
 }
 
