bad9adc4406db15ff7b000d0016d90c8d0e1fc70
JaneLab <janelab@DHCP-22241.iis.sinica.edu.tw>
2019-10-05 18:34:47 +0800

    add the component of ToastNotification

---
+++ src/js/components/presentational/ToastNotification.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { AppContext } from '../../context/AppContext';
+import moment from 'moment'
+import config from '../../config'
+
+const ToastNotification = ({
+    data
+}) => {
+
+    const { locale }= React.useContext(AppContext);
+    return (
+        <div>
+            {data.name} {data.location_description} {moment(data.geofence_violation_timestamp).format(config.geoFenceViolationTimeFormat)}
+        </div>
+    )
+}
+
+export default ToastNotification
