609753a60c768a94f6a45e304afc93a192218ecf
JaneLab <janelab@JaneLabdeiMac.local>
2019-10-21 10:13:32 +0800

    reset the src of css style sheet of bootstrap

--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-/** Bootstrap Custom CSS */
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-
 /** Import Custom CSS */
 import './js/customizedCSS';
 
@@ -13,6 +10,7 @@ import App from './App'
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(<App />, rootElement);
+
 
 
 
