791a09492ef01923a479c1691943fbb598251ea4
JaneLab <janelab@DHCP-22241.iis.sinica.edu.tw>
2020-01-02 11:12:30 +0800

    add reserve function in ConfirmForm

--- src/js/components/presentational/SearchResultList.js
+++ src/js/components/presentational/SearchResultList.js
@@ -152,7 +152,6 @@ class SearchResult extends React.Component {
 
         /** Create the pdf package, including pdf, pdf setting and path */
         let pdfPackage = shouldCreatePdf && config.getPdfPackage(status, auth.user, this.state.editedObjectPackage, locale)
-        
         axios.post(dataSrc.editObjectPackage, {
             formOption: editedObjectPackage,
             username,
