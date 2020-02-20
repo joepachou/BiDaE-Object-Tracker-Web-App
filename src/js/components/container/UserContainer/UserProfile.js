0a780f56ecd386a6971d2f554eb5503b57297027
happy955201314 <chiwei955201314@gmail.com>
2020-02-21 03:53:50 +0800

    upload

--- src/js/components/container/UserContainer/UserProfile.js
+++ src/js/components/container/UserContainer/UserProfile.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Image} from 'react-bootstrap';
+import { Button, Image, ListGroup} from 'react-bootstrap';
 import userProfileImg from '../../../../img/icon/userProfile.png';
 import { AppContext } from '../../../context/AppContext';
 import ImageUploader from 'react-images-upload';
@@ -16,10 +16,12 @@ import {
 import {
     Row,
     Col,
+    Modal,
+    InputGroup
 } from "react-bootstrap"
 import { Select } from 'semantic-ui-react';
 import NumberPicker from '../NumberPicker';
-
+import cloneDeep from 'lodash/cloneDeep';
 
 let elseArray = []
 let  rows_data = []
@@ -30,58 +32,31 @@ class UserProfile extends React.Component{
     static contextType = AppContext
 
     state= {
+        show: false,
         locale: '',
         userData:[],
         mainArea:'',
         elseArea:[],
-        columns: [
-            {
-              label: 'Area',
-              field: 'area',
-              sort: 'asc',
-            },
-            {
-                label: 'Add',
-                field: 'add',
-                sort: 'asc'
-              },
-            {
-              label: 'Remove',
-              field: 'remove',
-              sort: 'asc'
-            },
-            {
-                label: 'Note',
-                field: 'note',
-                sort: 'asc'
-              }
-           
-          ],
-        settingColumns: [
-          {
-            label: 'Settings',
-            field: 'setting',
-            sord: 'asc'
-          },
-          {
-            label: '',
-            field: 'choice',
-            sort: 'asc'
-          }
-        ],
-        rows_data:[],
-        userInfo: null
+        userInfo: null,
+        upadateAreaId: [],
 
+        totalAreaId: [],
+        
+        secondaryAreaId: [],
+        secondaryAreaIdBeforUpdate:[],
+        
+        otherAreaId: [],
+        otherAreaIdBeforUpdate: []
     }
 
     componentDidUpdate = (prevProps, prevState) => {
-        if (this.context.locale.abbr !== prevState.locale) {
-            this.setState({
-                locale: this.context.locale.abbr,
-                rows_data:[]
-            })
-            this.getAreaTable();
-        }
+        // if (this.context.locale.abbr !== prevState.locale) {
+        //     this.setState({
+        //         locale: this.context.locale.abbr,
+        //         rows_data:[]
+        //     })
+        //     this.getAreaTable();
+        // }
     }
 
     componentDidMount = () => {
@@ -95,200 +70,200 @@ class UserProfile extends React.Component{
         this.getAreaTable()
       }
    
-      getUserData =() =>{
-        const {auth} = this.context
-        axios.post(getUserArea, {
-            user_id: auth.user.id
-        })
-        .then(res => {
-
-           let updataAuthAreas_ID = [] //因為auth裡面是陣列 res抓出來是json 轉一下
-           res.data.rows.map(item=>{
-               updataAuthAreas_ID.push(item.area_id)
-           })
-           auth.user.areas_id =updataAuthAreas_ID
-
-           this.setState({
-                userData : res.data.rows
-            })
-
-        })
-        .catch(err => {
-            console.log(err)
+    getUserData =() =>{
+    const { 
+        areaOptions
+    } = config.mapConfig
+    const {auth} = this.context
+    let userIdList = []
+    axios.post(getUserArea, {
+        user_id: auth.user.id
+    })
+    .then(res => {
+    
+        res.data.rows.map(item=>{
+            userIdList.push(item.area_id)
         })
 
-      }
+        auth.user.areas_id = userIdList
+
+        this.setState({
+            userData : res.data.rows,
+            secondaryAreaId : userIdList
+        })
+    })
+    .catch(err => {
+        console.log(err)
+    })
+    
+    }
 
     getAreaTable = () => {
         let { locale } = this.context
         const {auth} = this.context
+        const { 
+            areaOptions
+        } = config.mapConfig
         rows_data = []
-
-        this.setState({
-            rows_data
-        })
 
         axios.post(getAreaTable, {
             locale: locale.abbr
         })
         .then(res => {
-            axios.post(getUserArea, {
-            user_id: auth.user.id
-        })
-        .then(res_UserArea => {
-            let noteCount =0;
-            let mainAreaBtn = []
-            let noteArray = []
-            res_UserArea.data.rows.map(nowArea => {
-                noteArray[parseInt(nowArea.area_id)] =locale.texts.ALREADY_CHOOSE
+            let totalAreaIdList = []
+
+
+            res.data.rows.map( item =>{
+                totalAreaIdList.push(item.id)
             })
-            noteArray[parseInt(auth.user.main_area)] = locale.texts.MAIN_AREA
-            mainAreaBtn[parseInt(auth.user.main_area)] = true
-        
-            if (rows_data == ''){//避免有兩個一樣的資料，像是２筆變４筆
-                res.data.rows.map(item=>{
-                    if (process.env.SITES_GROUP.includes(parseInt(item.id)) ) {
-                        noteCount +=1    
-                        rows_data.push({
-                        area : locale.texts[item.name],
-                        add :   <Button  name={item.name} disabled={mainAreaBtn[noteCount]} variant="outline-success" className="text-capitalize"  onClick={this.handleAddArea}>{locale.texts.ADD}</Button>,
-                        remove :   <Button  name={item.name} disabled={mainAreaBtn[noteCount]}  variant="outline-info" className="text-capitalize"  onClick={this.handleRemoveArea}>{locale.texts.REMOVE}</Button>,
-                        note : noteArray[noteCount]
-                        })
+            this.setState({
+                totalAreaId: totalAreaIdList,
+            })
+            let otherAreaIdList = []
+            this.state.totalAreaId.map( item => {
+                let check = true
+                this.state.secondaryAreaId.map( item2=> {
+                    if( item == item2){
+                        check = false
                     }
                 })
-                this.setState({
-                    rows_data
-                }) 
-            }
-        
-        })
-        .catch(err => {
-            console.log(err)
-        })
-        })
-        .catch(err => {
-            console.log(err)
-        })
-    }
-    resetFreqSearchCount = (value) => {
-        const {
-            auth
-        } = this.context;
-        
-        if (value) {
-            let userInfo = this.state.userInfo
-            userInfo.freqSearchCount = value
+                if(check == true){
+                    otherAreaIdList.push(item)
+                }
+            })
+
             this.setState({
-                userInfo: userInfo
+                otherAreaId: otherAreaIdList,
             })
-            axios.post(modifyUserInfo, {
-                info: userInfo,
-                username: userInfo['name']
-            }).then(res => {
-                auth.setUserInfo('freqSearchCount', value)
-            }) 
-        }
+
+        })
+        .catch(err => {
+            console.log(err)
+        })
     }
 
-    handleAddArea =(e) =>{
-        let { locale } = this.context
-        let { name } = e.target
-        const {auth} = this.context;
-        this.state.elseArea.includes(name) ? null :   elseArray.push(name)
-        this.setState({
-            elseArea:elseArray,
+    // resetFreqSearchCount = (value) => {
+    //     const {
+    //         auth
+    //     } = this.context;
+        
+    //     if (value) {
+    //         let userInfo = this.state.userInfo
+    //         userInfo.freqSearchCount = value
+    //         this.setState({
+    //             userInfo: userInfo
+    //         })
+    //         axios.post(modifyUserInfo, {
+    //             info: userInfo,
+    //             username: userInfo['name']
+    //         }).then(res => {
+    //             auth.setUserInfo('freqSearchCount', value)
+    //         }) 
+    //     }
+    // }
+
+    handleAddArea =(areaId) =>{
+
+        const { 
+            locale,
+            auth 
+        } = this.context
+
+        axios.post(addUserArea, {
+            user_id: auth.user.id,
+            area_id: areaId
         })
-
-    
-        let repeatFlag = false;
-        this.state.userData.map(item =>{
-            item.area_id == config.mapConfig.areaList.indexOf(name) ? repeatFlag = true : null
+        .then(res => {
+            console.log('query:已新增,' + areaId )
+            this.getUserData()
         })
-        if (repeatFlag != true) { // 還沒新增到db
-            axios.post(addUserArea, {
-                user_id: auth.user.id,
-                area_id:config.mapConfig.areaList.indexOf(name)
-            })
-            .then(res => {
-                console.log('query:已新增,' + config.mapConfig.areaList.indexOf(name) )
-                this.getUserData()
-            })
-            .catch(errr => {
-                console.log(errr)
-            })
-        }else{
-            console.log('query:已在當前區域')
-        }
-
-        let itemCount = 1
-        rows_data = this.state.rows_data
-        rows_data.map(item=>{
-              if(itemCount == config.mapConfig.areaList.indexOf(name)){
-                   item.note = locale.texts.ALREADY_CHOOSE
-              }
-             itemCount = itemCount+1
+        .catch(errr => {
+            console.log(errr)
         })
-        this.setState({rows_data})
-
-
     }
 
-    handleRemoveArea =(e) =>{
-        let { name } = e.target
-        const {auth} = this.context;
-        this.state.elseArea.includes(name) ? elseArray.splice(elseArray.indexOf(name),1) :  null
+    handleRemoveArea = (areaId) =>{
+
+        const { 
+            locale,
+            auth 
+        } = this.context
+
+        axios.post(DeleteUserArea, {
+            user_id: auth.user.id,
+            area_id: areaId
+        })
+        .then(res => {
+            console.log('query:已刪除,' + areaId )
+            this.getUserData()
+        })
+        .catch(errr => {
+            console.log(errr)
+        })
+    }
+
+    handleShowModal = () => {
+        
         this.setState({
-            elseArea:elseArray,
+            secondaryAreaIdBeforUpdate : this.state.secondaryAreaId,
+            otherAreaIdBeforUpdate: this.state.otherAreaId,
+            show: true
+        })
+    }
+
+    handleCloseModal = () => {
+        this.setState({
+            show: false
+        })
+    }
+
+    handleCloseModalwithSave = () => {
+        this.state.secondaryAreaIdBeforUpdate.map(item => {
+            var check = true
+            this.state.secondaryAreaId.map(item2 => {
+                if( item == item2) check = false
+            })
+            if(check == true)　this.handleAddArea(item)
         })
 
-    //    console.log(auth)
-    //    console.log(auth.user.areas_id.indexOf(name))        
-    //     let deleteCount = 0;
-    //     auth.user.areas_id.map(item=>{
-    //         console.log(item + '==' + name)
-    //         if (item == config.mapConfig.areaList.indexOf(name))
-    //         {
-    //           delete auth.user.areas_id[deleteCount]  
-    //         }
-    //         deleteCount +=1
-    //     })
-    //     console.log(auth)
-
-       let isDeletePresence = false; // 當前擁有的地區權限有沒有目前要移除的點
-           
-           this.state.userData.map(item =>{
-               item.area_id == config.mapConfig.areaList.indexOf(name) ? isDeletePresence = true : null
-           })
-
-
-           if (isDeletePresence == true) { // 有在列表裡 欠刪
-            axios.post(DeleteUserArea, {
-                user_id: auth.user.id,
-                area_id:config.mapConfig.areaList.indexOf(name)
+        this.state.secondaryAreaId.map(item => {
+            var check = true
+            this.state.secondaryAreaIdBeforUpdate.map(item2 => {
+                if( item == item2) check = false
             })
-            .then(res => {
-                console.log('query:已刪除,' + config.mapConfig.areaList.indexOf(name) )
-                this.getUserData()
-            })
-            .catch(errr => {
-                console.log(errr)
-            })
-        }else{
-            console.log('query:未在當前區域')
-        }
- 
-
-        let itemCount = 1
-        rows_data = this.state.rows_data
-        rows_data.map(item=>{
-              if(itemCount == config.mapConfig.areaList.indexOf(name)){
-                   item.note = ''
-              }
-             itemCount = itemCount+1
+            if( check == true) this.handleRemoveArea(item)
         })
-        this.setState({rows_data})
+        this.setState({
+            show: false
+        })
+    }
 
+    handleBeforUpdateAdd = (e) => {
+        let chooseList = cloneDeep(this.state.secondaryAreaIdBeforUpdate)
+        let didntchooseList = cloneDeep(this.state.otherAreaIdBeforUpdate)
+
+        chooseList.push(e)
+        var indx = didntchooseList.indexOf(e)
+        didntchooseList.splice(indx,1)
+
+        this.setState({
+            secondaryAreaIdBeforUpdate : chooseList,
+            otherAreaIdBeforUpdate: didntchooseList
+        })
+    }
+
+    handleBeforUpdateDele = (e) => {
+        let chooseList = cloneDeep(this.state.secondaryAreaIdBeforUpdate)
+        let didntchooseList = cloneDeep(this.state.otherAreaIdBeforUpdate)
+        
+        didntchooseList.push(e)
+        var indx = chooseList.indexOf(e)
+        chooseList.splice(indx,1)
+        
+        this.setState({
+            secondaryAreaIdBeforUpdate : chooseList,
+            otherAreaIdBeforUpdate: didntchooseList
+        })
     }
 
     render(){
@@ -323,27 +298,65 @@ class UserProfile extends React.Component{
                     <div className="title ">
                         {locale.texts.YOUR_SERVICE_AREAS}
                     </div>
+                    <Modal show={this.state.show} onHide={this.handleCloseModal}>
+                        <Modal.Header closeButton>
+                        <Modal.Title>編輯次要區域</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            <h4>以選取區域</h4>
+                            <ListGroup>
+                            {
+                                this.state.secondaryAreaIdBeforUpdate.map((item,index) => {
+                                    let element = 
+                                        <ListGroup.Item
+                                            key = {index}
+                                        >
+                                            {locale.texts[areaOptions[item]]}
+                                            <Button variant='link' onClick={()=>this.handleBeforUpdateDele(item)}>刪除</Button>
+                                        </ListGroup.Item>
+                                    return element
+                                })
+                            }
+                            </ListGroup>
+                            <h4>未選取區域</h4>
+                            <ListGroup>
+                            {
+                                this.state.otherAreaIdBeforUpdate.map((item,index) => {
+                                    let element = 
+                                        <ListGroup.Item
+                                            key = {index}
+                                        >
+                                            {locale.texts[areaOptions[item]]}
+                                            <Button variant='link' onClick={()=>this.handleBeforUpdateAdd(item)}>新增</Button>
+                                        </ListGroup.Item>
+                                    return element
+                                })
+                            }
+                            </ListGroup>
+                        </Modal.Body>
+                        <Modal.Footer>
+                        <Button variant="secondary" onClick={this.handleCloseModal}>
+                            Close
+                        </Button>
+                        <Button variant="primary" onClick={this.handleCloseModalwithSave}>
+                            Save Changes
+                        </Button>
+                        </Modal.Footer>
+                    </Modal>
                     <div>
                         <p>
                             {locale.texts.MAIN_AREA}: {locale.texts[areaOptions[auth.user.main_area]]}
                         </p>
                         <p>
                             {locale.texts.SECONDARY_AREAS}: {
-                                auth.user.areas_id.map(id => {
+                                this.state.secondaryAreaId.map(id => {
                                     return locale.texts[areaOptions[id]]
                                 }).join('/')
                             }
+                            <Button variant='link' onClick={this.handleShowModal}>編輯</Button>
                         </p>
                     </div>
                 </div>
-                <Row className="m-3" style={{width: '100%'}}>
-                    <Col>
-                        <MDBTable>
-                        <MDBTableHead columns={this.state.columns} />
-                        <MDBTableBody rows={this.state.rows_data} />
-                        </MDBTable>
-                    </Col>
-                </Row>
                 <hr/>
                 <div>
                     <div 
