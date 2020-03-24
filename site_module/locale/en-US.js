6e6f9c14c9bda7a8c55efd0a4833eabd8cd2f4f0
JaneLab <janelab@DHCP-22030.iis.sinica.edu.tw>
2020-03-24 13:57:18 +0800

    set template of site module

--- site_module/locale/en-US.js
+++ site_module/locale/en-US.js
@@ -11,13 +11,6 @@ const en = {
      * AREA_NAME_IN_AREA_TABLE_OF_DATABASE: YOUR_CUSTOMIZED_LOCALE_NAME
     */
 
-    IIS_SINICA_FOURTH_FLOOR: "IIS sinica floor 4",
-    NTUH_YUNLIN_WARD_FIVE_B: "ward 5B",
-    VETERAN_HOME_FIRST_FLOOR: "veteran home floor 1",
-    VETERAN_HOME_THIRD_FLOOR: "veteran home floor 3",
-    NTUH_EMERGENCY_ROOM: "emergency room",
-    YUANLIN_CHRISTIAN_HOSPITAL: "Yuanlin Christian Hospital",
-    NURSING_HOME: "nursing home",
     EMERGENCY_ROOM: "emergency room",
     WHOLE_SITE: "whole site"
 
