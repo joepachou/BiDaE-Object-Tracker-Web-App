0c604d02515326ea198488996cba781da9749ab8
FizzedYA <LT1stSoloMID@gmail.com>
2020-04-30 13:25:05 +0800

    api compliction

--- web_server/api_query.js
+++ web_server/api_query.js
@@ -1,6 +1,6 @@
+const error_code = require ('./api_error_code');  
 require('moment-timezone')
-const moment = require('moment');
-const sha256 = require('sha256');
+const moment = require('moment'); 
 const bcrypt = require('bcrypt');
 const queryType = require ('./api_queryType');
 const pg = require('pg');
@@ -12,6 +12,7 @@ const config = {
     port: process.env.DB_PORT,
 }
 const pool = new pg.Pool(config)  
+const timeDefaultFormat = 'YYYY/MM/DD HH:mm:ss'
 
 const get_api_key = (request, response) => {
     let { 
@@ -22,38 +23,32 @@ const get_api_key = (request, response) => {
 
     const saltRounds = 10; 
     let getUserName = ''
-    pool.query(queryType.getAllUser())//驗證帳號的正確並找出name
+    pool.query(queryType.getAllUser())//verification by sha256
     .then(res =>{
         res.rows.map(item =>{ 
             if (username == item.username_sha256 && password == item.password_sha256){
                 getUserName = item.name
             }
         })   
-        if (getUserName != ''){
+        if (getUserName != ''){//already match user name
             pool.query(queryType.confirmValidation(getUserName))
             .then(res => {   
                     console.log(`confirm validation succeed`)  
                     const hash = bcrypt.hashSync(username, saltRounds); 
-                    pool.query(queryType.setKey(res.rows[0].user_id,username,hash)) 
+                    pool.query(queryType.setKey(res.rows[0].user_id,getUserName,hash)) 
                         .then(res => {  
-                            response.status(200).json({
-                                error_code: '0',
-                                error_message:'get key success',
-                                key: hash,
-                                note:'validity period of key until : ' + moment().add(300, 'm').locale('en').format('LT')
-                            }) 
-                            console.log('set Key success')  
+                            response.json(error_code.get_key_success(hash,moment().add(30, 'm').locale('en').format('LT')))
+                            console.log('get Key success')  
                         })
                         .catch(err => {
                             console.log(`set Key failer ${err}`)
-                    })    
-                    console.log(`get key succeed`)   
+                    })     
             })
             .catch(err => {
-            console.log(`confirm validation fails ${err}`)
+                 console.log(`confirm validation fails ${err}`)
             })   
         }else{
-            console.log(`username or pwd fail`)
+            response.json(error_code.sha_256_incorrect)   
         } 
     })
     .catch(err => {
@@ -62,96 +57,119 @@ const get_api_key = (request, response) => {
  
 }
 
-
 async function get_history_data(request, response){
 
     let { 
         key,
-        tag, // array
-        Lbeacon, // array
+        tag, // string
+        Lbeacon, // string
         start_time, // YYYY/MM/DD HH:mm:ss
         end_time, // YYYY/MM/DD HH:mm:ss
         count_limit, // 
         sort_type,
     } = request.body 
-    
-    tagFilter =[]
-    LbeaconFilter = []
-    tag.map(tagItem=>{tagFilter.push(tagItem.replace(/`'/g, ""))  }) 
-    Lbeacon.map(LbeaconItem=>{LbeaconFilter.push(LbeaconItem.replace(/`'/g, ""))  }) 
-     
- 
+      
     var matchRes = ( Promise.resolve(match_key(key)));
     await   matchRes.then(function(result){matchRes = result}) 
-
-    if (matchRes == 1 ){   // 金鑰驗證通過
-
-        // 檢查這把金鑰的所屬地區 
-        area = (Promise.resolve(get_area_role(key)));
-        await  area.then(function(result){area = result}) 
-
-        //找出出屬於此區域的mac_address －> 並依照輸入的資料再過濾
-        var filterObject = (Promise.resolve(filterMacAddressByArea(area,tagFilter)));
-        await  filterObject.then(function(result){filterObject = result}) 
- 
-        //用過濾後的mac_address 配合LBeacon 找出history
-        let data  = (Promise.resolve(get_history(filterObject,LbeaconFilter)));
-        await  data.then(function(result){data = result})  
+    
+    if (matchRes == 1 ){   // matched
 
 
-        let temp      
-        temp = (Promise.resolve(filterData_ByTime(data,start_time,end_time)));
-        await  temp.then(function(result){temp = result}) 
-        data = temp
- 
+
+        //** Time **//
+        
+        if(start_time != undefined) { 
+            // verification by format
+            if (moment(start_time, timeDefaultFormat, true).isValid() == false){
+                response.json(error_code.start_time_error)  
+            }else{ // if format right then convert to utc
+                start_time =  time_format(start_time) 
+            }
+        }else{ // set default WHEN no input
+            start_time = moment.tz(moment().subtract(365,'day') , process.env.TZ).format(process.env.TIMESTAMP_FORMAT)
+        }
+
+        if(end_time != undefined) {
+            if (moment(end_time, timeDefaultFormat, true).isValid() == false){
+                response.json(error_code.end_time_error)  
+            }else{
+                end_time =  time_format(end_time) 
+            }
+        }else{
+            end_time = moment.tz(moment().format() , process.env.TZ).format(process.env.TIMESTAMP_FORMAT) 
+        }
+   
+
+        //** TAG **//
+        if (tag != undefined){
+            tag = tag.split(',')
+            let pattern = new RegExp("^[0-9a-fA-F]{2}:?[0-9a-fA-F]{2}:?[0-9a-fA-F]{2}:?[0-9a-fA-F]{2}:?[0-9a-fA-F]{2}:?[0-9a-fA-F]{2}$");
+            tag.map(item =>{ 
+                if(item.match(pattern) == null){//judge format
+                    response.json(error_code.mac_address_error)   
+                }       
+            })
+        }
+        
+        //** Lbeacon **//
+        if (Lbeacon != undefined){
+            Lbeacon = Lbeacon.split(',')
+            let pattern = new RegExp("^[0-9A-Fa-f]{8}-?[0-9A-Fa-f]{4}-?[0-9A-Fa-f]{4}-?[0-9A-Fa-f]{4}-?[0-9A-Fa-f]{12}$");
+            Lbeacon.map(item =>{ 
+                if(item.match(pattern) == null){//judge format
+                    response.json(error_code.Lbeacon_error)   
+                }      
+            })
+        }
+    
+        //set default when no input
+        if(count_limit == undefined){
+            count_limit = 10
+        }else{
+            isNaN(count_limit) ? response.json(error_code.count_error)  : null
+        } 
+
+        //0=DESC 1=ASC  : default=0 
+        if(sort_type == undefined){
+            sort_type = 'desc'
+        }else{ 
+            if (sort_type != 'desc' && sort_type != 'asc') {
+                response.json(error_code.sort_type_define_error)
+            } 
+        } 
+        
+        data = (Promise.resolve(get_data(key,start_time,end_time,tag,Lbeacon,count_limit,sort_type)));
+        await  data.then(function(result){data = result})   
+        
+        data.map(item=>{
+            item.start_time = moment(item.start_time).format(timeDefaultFormat)
+            item.end_time = moment(item.end_time).format(timeDefaultFormat)
+        })
+        
+        response.json(data)  
   
-        //預設是抓 "最新" "10" 筆
-        //有過濾資料條件：count limit 
-        temp = (Promise.resolve(filterData_Bylimit(data,count_limit,sort_type)));
-        await  temp.then(function(result){temp = result}) 
-        data = temp 
 
-
-        response.status(200).json(data)  
- 
-        
-
-    }else if (matchRes == 2 ){
-        response.json({
-            error_code: '201',
-            error_message:'get data fail : key is out of active time',
-            data: ''
-        })
-    }else{ // 金鑰驗證失敗 
-        
-        response.json({
-            error_code: '200',
-            error_message:'get data fail : key is incorrect',
-            data: ''
-        })
+    }else if (matchRes == 2 ){  
+        response.json(error_code.key_timeout)
+    }else{ // key fail match with user
+        response.json(error_code.key_incorrect) 
     } 
 }
  
 
 
-
-
-
  
 async function match_key(key, response){  
-    let matchFlag = 0
+    let matchFlag = 0 // flag = 0 when key error
     return await pool.query(queryType.getAllKey()) 
         .then(res => {    
             res.rows.map(item =>{ 
-                let vaildTime = moment(item.register_time).add(300, 'm')
-                
-                //時間內，且key正確
+                let vaildTime = moment(item.register_time).add(300, 'm')  
                 if (moment().isBefore(moment(vaildTime)) && item.key == key ){ 
-                   matchFlag = 1  
+                   matchFlag = 1  //in time & key right
                 } else if(moment().isAfter(moment(vaildTime)) && item.key == key ){
-                   matchFlag = 2
-                }
-                //key正確,時間超出 
+                   matchFlag = 2 // out time & key right
+                } 
             })    
             return matchFlag
         })
@@ -159,105 +177,27 @@ async function match_key(key, response){
             console.log(`match key fails ${err}`)
         })  
 } 
-
-async function get_area_role(key, response){    
-    return await pool.query(queryType.getAreaIDByKey(key))  //get area id
-        .then(res => {     
-            console.log(`get area role success`)   
-            return res.rows
-        })
-        .catch(err => { 
-            console.log(`get area role fails ${err}`)
-        })  
-} 
-
-async function filterMacAddressByArea(area  ,tag, response){    
-    
-    return await   pool.query(queryType.filterMacAddressByArea(area,tag)) 
-        .then(res => {    
-            console.log(`filter Mac Address success`) 
-            return res.rows
-        })
-        .catch(err => { 
-            console.log(`filter Mac Address ${err}`)
-        })  
-
-} 
-
-async function get_history(filterObject, Lbeacon,response){    
-    return await   pool.query(queryType.searchByMacAddress(filterObject,Lbeacon))
-    .then(res => { 
-        res.error_code= '0'
-        res.error_message='get data success'  
-
-        console.log(`search by mac address success`)    
-        return res.rows
-    })
-    .catch(err => { 
-        console.log(`search by mac address fail ${err}`)
-        response.json({
-            error_code: '300',
-            error_message:'get data fail : no object for this area',
-            data: ''
-        })
-    }) 
-}  
-
-async function filterData_ByTime(data,start_time,end_time){    
-    var filterRes = []
-    var filterResFinal = []  
-    if(start_time != undefined)
-    { 
-        try{
-            data.map(dataItem=>{
-                    if (moment(dataItem.record_timestamp).isAfter(moment(start_time,'YYYY/MM/DD HH:mm:ss').format()) ){ 
-                        filterRes.push(dataItem)
-                    } 
-            })
-        }catch(error){ 
-            console.log(`filter data by start_time fail ${err}`)
-        }
-    }else{
-        filterRes = data  
-    }
-
-    if(end_time != undefined)
-    { 
-        try{
-            filterRes.map(dataItem=>{ 
-                    if (moment(dataItem.record_timestamp).isBefore(moment(end_time,'YYYY/MM/DD HH:mm:ss').format()) ){ 
-                        filterResFinal.push(dataItem)
-                    } 
-            })
-        }catch(error){
-            console.log(`filter data by end_time fail ${err}`)
-        }    
-    }else{
-        filterResFinal = filterRes
-    }
-
-    return filterResFinal
-}  
-
-async function filterData_Bylimit(data,count_limit,sort_type){    
-    var filterRes = [],count = 0  
-    count_limit == undefined ? count_limit = 10 : null
-    sort_type == undefined ? data = data.reverse() :null 
-  
-    try{
-        data.map(dataItem=>{
-            count < count_limit ?  filterRes.push(dataItem) : null
-            count ++                 
-        })
-    }catch(error){
-        console.log('filter data by limit fail')
-    } 
-     
-    return filterRes
-} 
-
-
  
+
+async function get_data(key,start_time,end_time,tag,Lbeacon,count_limit,sort_type){    
+    return await pool.query(queryType.get_data(key,start_time,end_time,tag,Lbeacon,count_limit,sort_type))  //get area id
+        .then(res => {      
+            console.log(`get_data success`)   
+            return res.rows
+        })
+        .catch(err => { 
+            console.log(`get_data fails ${err}`)
+        })  
+} 
+
+
+function time_format(time){    
+    if (time != undefined){ 
+     return  moment.tz(moment(time,timeDefaultFormat).format() , process.env.TZ).format(process.env.TIMESTAMP_FORMAT)
+    }
+} 
+ 
+
 
 module.exports = {
     get_api_key,
--- web_server/api_queryType.js
+++ web_server/api_queryType.js
@@ -75,103 +75,118 @@ function setKey (user_id,username,hash) {
 }
 
 const getAllKey = () => { 
-    const query = 
-		` 
-			SELECT  *
-            FROM api_key 
-		` 
-	return query;
+    return query = ` SELECT  * FROM api_key `  
 }
 
 
-const getAreaIDByKey = (key) =>{ 
-	const text = `
-            SELECT area_id
-            FROM user_area
-            WHERE user_area.user_id =     
-            (
-                SELECT id 
-                FROM api_key 
-                WHERE key=$1
-            )
-	`
-	const values = [
-       key
-	];
-
-	const query = {
-		text,
-		values
-	};
-    
-	return query;
-}
-
-const searchByMacAddress  = (mac_address,Lbeacon) => {   
-    if (Lbeacon != undefined){
-        const query =  `
-            SELECT *  FROM location_history_table 
-            WHERE
-            (  
-            mac_address IN (${mac_address.map(item => `'${item.mac_address}'`)})
-            AND
-            uuid IN (${Lbeacon.map(item => `'${item}'`)})
-            )
-        `    
-    
-        return query
-    }  else{
-        const query =  `
-            SELECT *  FROM location_history_table 
-            WHERE 
-            mac_address IN (${mac_address.map(item => `'${item.mac_address}'`)})
-        `    
-        return query
-    }
-}
-
-const filterMacAddressByArea  = (area_id,tag) => { 
-    if (tag != undefined){
-            const query = 
-                `
-                SELECT mac_address  FROM object_table
-                WHERE  
-                (
-                area_id IN (${area_id.map(item => `'${item.area_id}'`)}) 
-                AND
-                mac_address IN  (${tag.map(item => `'${item}'`)})  
-                )
-                `     
-            
-            return query;
-    }else{
-        const query = 
-            `
-            SELECT mac_address  FROM object_table
-            WHERE   
-            area_id IN (${area_id.map(item => `'${item.area_id}'`)})  
-            `    
-         return query
-    }
-
-   
-}
-
-const getAllUser = () => { 
-    const query = 
-		` 
-			SELECT  *
-            FROM user_table
-		` 
-	return query;
+const getAllUser = () => {  
+    return query = ` SELECT  * FROM user_table	`  
 }
  
+const get_data =(key,start_time,end_time,tag,Lbeacon,count_limit,sort_type) =>{ 
+  let text = 
+    `
+    WITH ranges AS (
+        SELECT mac_address, area_id, uuid, record_timestamp, battery_voltage, average_rssi, 
+            CASE WHEN LAG(uuid) OVER 
+                    (PARTITION BY mac_address ORDER BY mac_address, record_timestamp) = uuid 
+                    THEN NULL ELSE 1 END r
+        FROM 
+        (			 
+            SELECT 
+                location_history_table.mac_address AS mac_address,
+                location_history_table.area_id AS area_id,
+                location_history_table.uuid AS uuid,
+                location_history_table.record_timestamp AS record_timestamp,
+                location_history_table.battery_voltage AS battery_voltage,
+                location_history_table.average_rssi AS average_rssi
+            FROM location_history_table 
+
+            INNER JOIN object_table
+            ON location_history_table.mac_address = object_table.mac_address
+                AND object_table.type = 'Patient'
+
+            INNER JOIN user_area
+            ON object_table.area_id = user_area.area_id
+
+            INNER JOIN user_table 
+            ON user_table.id = user_area.user_id
+
+            INNER JOIN api_key
+            ON api_key.name = user_table.name
+            AND api_key.key = $3
+            WHERE 
+                record_timestamp > $1
+                AND record_timestamp < $2`;
+                if(tag != undefined){
+                    text+=`  AND location_history_table.mac_address IN  (${tag.map(item => `'${item}'`)})`
+                } 
+                if(Lbeacon != undefined){
+                    text+=`  AND location_history_table.uuid IN  (${Lbeacon.map(item => `'${item}'`)})`
+                }  
+       text+=`) AS raw_data
+    )
+
+    , groups AS (
+        SELECT mac_address, area_id, uuid, record_timestamp, battery_voltage, average_rssi, r, 
+            SUM(r) 
+                OVER (ORDER BY mac_address, record_timestamp) grp
+        FROM ranges
+    )
+
+    SELECT 
+        MIN(groups.mac_address::TEXT) AS mac_address, 
+        MIN(object_table.name) AS name,
+        MIN(groups.area_id) AS area_id,
+        MIN(area_table.readable_name) AS area_name,
+        MIN(groups.uuid::TEXT) AS uuid,
+        MIN(Lbeacon_table.description) AS beacon_description,
+        MIN(groups.battery_voltage) AS battery_voltage,
+        AVG(groups.average_rssi) AS avg_rssi,
+        MIN(groups.record_timestamp) AS start_time,
+        MAX(groups.record_timestamp) AS end_time,
+        MAX(groups.record_timestamp) - MIN(groups.record_timestamp) AS duration
+    FROM groups
+
+    INNER JOIN object_table
+    ON object_table.mac_address = groups.mac_address
+
+    INNER JOIN area_table
+    ON area_table.id = groups.area_id
+
+    INNER JOIN Lbeacon_table
+    ON Lbeacon_table.uuid = groups.uuid
+
+    GROUP BY grp, groups.mac_address
+    `;
+
+    if(sort_type == 'desc'){
+        text +=`  ORDER by mac_address ASC, start_time DESC   `;
+    }else{
+        text +=`   ORDER by mac_address ASC, start_time ASC   `;
+    }
+    text +=` LIMIT  $4`
+ 
+     
+	const values = [
+        start_time,
+        end_time,
+        key,
+        count_limit
+     ];  
+     const query = {
+         text,
+         values
+     }; 
+
+  return query
+
+}
+
 module.exports = {
     confirmValidation,
     setKey,
-    getAllKey,
-    searchByMacAddress,
-    getAreaIDByKey,
-    filterMacAddressByArea ,
-    getAllUser
+    getAllKey, 
+    getAllUser, 
+    get_data
 }
