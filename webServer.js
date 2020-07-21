a68799527be48d9d9e739812eeb3b48cf4d13f65
Joe <joe@DHCP-22023.iis.sinica.edu.tw>
2020-07-21 16:45:30 +0800

    clean code in webServer

--- webServer.js
+++ webServer.js
@@ -5,16 +5,17 @@ const bodyParser = require('body-parser');
 const httpsPort = 443;
 const httpPort = 80;
 const path = require('path');
-const fs = require('fs')
 const https = require('https');
 const http = require('http');
 const session = require('express-session');
 const validation = require('./api/middlewares/validation');
 const sessionOptions = require('./api/config/session');
+const credentials = require('./api/config/credentials');
 const dataRoutes = require('./api/routes/dataRoutes');
 const authRoutes = require('./api/routes/dataRoutes/authRoutes');
-const UIRoutes = require('./web_server/routes/UIRoutes');
+const UIRoutes = require('./api/routes/UIRoutes');
 const APIRoutes = require('./web_server/routes/APIRoutes');
+
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true,}));
@@ -24,13 +25,11 @@ app.use(function(req, res, next) {
     next();
 });
 
-app.use(session(sessionOptions))
+app.use(session(sessionOptions));
 
 app.use(express.static(path.join(__dirname,'dist')));
 
-app.get(/^\/page\/(.*)/, (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist','index.html'));
-})
+UIRoutes(app);
 
 authRoutes(app);
 
@@ -38,37 +37,15 @@ authRoutes(app);
 // app.use(validation.authChecker);
 
 /** Data retrieving routes */
-dataRoutes(app)
-
-// UIRoutes(app);
+dataRoutes(app);
 
 APIRoutes(app);
 
+const httpsServer = https.createServer(credentials, app);
 
-/** privatekey name: private.key
- *  certificate name: certificate.cert or certificate.crt
- *  ca_bundle name: ca.bundle.crt
- */
-
-/** Create self-signed certificate  
- *  >> openssl req -nodes -new -x509 -keyout private.key -out certificate.cert 
- * If it is window os, please refer to https://tecadmin.net/install-openssl-on-windows/ install openssl 
- * and set the environment variables*/
-var privateKey = process.env.PRIVATE_KEY && fs.readFileSync(__dirname + `/ssl/${process.env.PRIVATE_KEY}`)
-var certificate = process.env.CERTIFICATE && fs.readFileSync(__dirname + `/ssl/${process.env.CERTIFICATE}`) 
-var ca_bundle = process.env.CA_BUNDLE && fs.readFileSync(__dirname + `/ssl/${process.env.CA_BUNDLE}`)
-
-var credentials = { 
-    key: privateKey, 
-    cert: certificate,
-    ca: ca_bundle
-}
-
-const httpsServer = https.createServer(credentials, app)
-
-/** Enable HTTP server */
+/** Initiate HTTPS server */
 httpsServer.listen(httpsPort, () => {
-    console.log(`HTTP Server running on PORT ${httpsPort}`)
+    console.log(`HTTPS Server running on PORT ${httpsPort}`)
 })
 
 httpsServer.timeout = parseInt(process.env.SERVER_TIMEOUT);
