3e3cc41846b5f576d859395903eb0f254ec3cd46
Joe <joe@DHCP-22023.iis.sinica.edu.tw>
2020-08-27 18:29:19 +0800

    adjust service worker and manifest

--- src/js/serviceWorker/sw.js
+++ src/js/serviceWorker/sw.js
@@ -36,7 +36,7 @@ import { precacheAndRoute } from 'workbox-precaching';
 import { registerRoute } from 'workbox-routing';
 import { CacheFirst } from 'workbox-strategies';
 
-// precacheAndRoute(self.__WB_MANIFEST);
+precacheAndRoute(self.__WB_MANIFEST);
 registerRoute (
     ({request}) => request.destination === 'image',
     new CacheFirst({
